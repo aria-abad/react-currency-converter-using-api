de317e59d534cb9c61b86854c6ee2e6e49d8b7ed
Aria <aria.abad2030@yahoo.com>
2021-03-13 12:01:24 -0800

    build the structure of the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import CurrencyRow from './components/CurrencyRow';
+
 
 function App() {
   return (
-    <h1>Currency Converter</h1>
+    <>
+      <h1>Currency Converter</h1>
+      <CurrencyRow />
+      <h1>&#x0296F;</h1>
+      <CurrencyRow />
+    </>
   );
 }
 
