94d7fc83cc958d8c480f1d1da31066e6c413a0a9
Aria <aria.abad2030@yahoo.com>
2021-03-13 13:59:31 -0800

    start fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 import CurrencyRow from './components/CurrencyRow';
 
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
 
 function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => setCurrencyOptions([data.base, ...Object.keys(data.rates)]))
+  }, [])
+
   return (
     <>
       <h1>Currency Converter</h1>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions= {currencyOptions} />
       <h1>&#x0296F;</h1>
-      <CurrencyRow />
+      <CurrencyRow currencyOptions={currencyOptions} />
     </>
   );
 }
